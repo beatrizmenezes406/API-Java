4977168c553458a8d3e423440c3ab4593a6974a4
Betriz Menezes <beatrizmenezes406@gmail.com>
2021-07-04 14:54:51 -0300

    Métodos DELETE e PUT

--- src/main/java/com/projetos/api/java/controller/ClienteController.java
+++ src/main/java/com/projetos/api/java/controller/ClienteController.java
@@ -1,11 +1,14 @@
 package com.projetos.api.java.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -28,8 +31,25 @@ public class ClienteController {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente adicionarclinete(@RequestBody Cliente cliente) {
+	public Cliente adicionarcliente(@RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
+	}
+	
+	@DeleteMapping
+	@ResponseStatus(HttpStatus.OK)
+	public String removercliente(@RequestBody Cliente cliente) {
+		clienteRepository.delete(cliente);
+		return "Delete Sucess!";
+	}
+	
+	@PutMapping
+	@ResponseStatus(HttpStatus.OK)
+	public String atualizarcliente(@RequestBody Cliente cliente) {
+		Cliente clienteToUpdate = clienteRepository.getById(cliente.getId());
+		clienteToUpdate.setNome(cliente.getNome());
+		clienteRepository.save(clienteToUpdate);
+		
+		return "Update Sucess!";
 	}
 
 }
